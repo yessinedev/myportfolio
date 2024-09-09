13527669882837f5ddeb3d71df4c830d0eb92dba
yessinedev <yessine.ag@gmail.com>
2024-09-09 21:48:37 +0100

    fix ui

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -8,7 +8,7 @@ import GitIcon from "@/assets/icons/git.svg";
 import TailwindIcon from "@/assets/icons/tailwindcss.svg";
 import CssIcon from "@/assets/icons/css3.svg";
 import { TechIcon } from "@/components/TechIcon";
-import MapImage from "@/assets/images/map.png";
+import MapImage from "@/assets/images/map2.png";
 import MemojiSmile from "@/assets/images/memoji-smile.png";
 import Image from "next/image";
 import { CardHeader } from "@/components/CardHeader";
@@ -61,21 +61,29 @@ const hobbies = [
   {
     title: "Movies",
     emoji: "🎬",
-    left: '10%',
+    left: '15%',
     top: '35%'
   },
   {
     title: "Music",
     emoji: "🎵",
-    left: '35%',
-    top: '40%'
+    left: '20%',
+    top: '70%'
   },
   {
     title: "Fitness",
     emoji: "🏋️‍♂️",
-    left: '70%',
-    top: '45%'
+    left: '40%',
+    top: '55%'
   },
+  {
+    title: "Reading",
+    emoji: "🏋️‍♂️",
+    left: '60%',
+    top: '30%'
+  },
+ 
+
 ];
 
 export const AboutSection = () => {
@@ -88,14 +96,14 @@ export const AboutSection = () => {
           description="Learn more about who I am, what I do, and what inspires me"
         />
         <div className="mt-20 flex flex-col gap-8">
-          <Card className="h-[320px] p-0">
+          <Card className="h-[320px] md:col-span-3">
             <CardHeader
               title="My Toolbox"
               description="Explore the technologies and tools I useto craft exceptional
                 digital experiences."
-              className="px-6 pt-6"
+              className=""
             />
-            <ToolboxItems tools={tools} className="mt-6" />
+            <ToolboxItems tools={tools} className="" />
             <ToolboxItems
               tools={tools}
               className="mt-6"
@@ -124,10 +132,12 @@ export const AboutSection = () => {
               ))}
             </div>
           </Card>
-          <Card>
-            <Image src={MapImage} alt="map" />
-            <Image src={MemojiSmile} alt="memoji" />
-          </Card>
+          {/* <Card className="h-[320px] p-0">
+            <Image src={MapImage} alt="map" className="h-full w-full object-cover object-left-top" />
+            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 size-20 rounded-full bg-gradient-to-r from-emerald-300 to-sky-400 after:content-[''] after:absolute after:inset-0 after:outline after:outline-2 after:-outline-offset-2 after:rounded-full after:outline-gray-950/30">
+            <Image src={MemojiSmile} alt="memoji" className="size-20" />
+            </div>
+          </Card> */}
         </div>
       </div>
     </div>
