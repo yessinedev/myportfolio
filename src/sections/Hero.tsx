cde398256ca55fc6f746eb0690b9c83950e35881
yessinedev <yessine.ag@gmail.com>
2024-09-09 20:41:56 +0100

    fix

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -70,7 +70,7 @@ export const HeroSection = () => {
           </h1>
           <p className="mt-4 text-center text-white/60 md:text-lg">
             I specialize in turning ideas into functional, high-performing web
-            applications. Let's discuss your next project.
+            applications. Let&apos;s discuss your next project.
           </p>
         </div>
         <div className="flex flex-col md:flex-row justify-center items-center mt-8 gap-4">
@@ -80,7 +80,7 @@ export const HeroSection = () => {
           </button>
           <button className="inline-flex items-center gap-2 border border-white bg-white text-gray-900 h-12 px-6 rounded-xl">
             <span>👋</span>
-            <span className="font-semiblod">Let's Connect</span>
+            <span className="font-semiblod">Let&apos;s Connect</span>
           </button>
         </div>
       </div>
